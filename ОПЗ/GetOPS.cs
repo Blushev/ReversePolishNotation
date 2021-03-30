f4c5f07762058e9b31887beda2c335e4b95ddd06
Egor <egor.rumyancev.9494@mail.ru>
2021-03-30 16:46:24 +0500

    Добавлен метод преобразования в ОПЗ

--- ОПЗ/GetOPS.cs
+++ ОПЗ/GetOPS.cs
@@ -6,6 +6,8 @@ namespace ОПЗ
 {
     class GetOPS
     {
+        List<char> str = new List<char>();
+        List<char> stac = new List<char>();
         public int Priority(char _prioritySign)
         {
             int priority = 0;
@@ -35,9 +37,67 @@ namespace ОПЗ
         }
 
 
-        public void SignAfter()
+        public void SignAfter(string _text)
         {
+            try
+            {
+                for(int i = 0; i < _text.Length; i++)
+                {
+                    if (int.TryParse(_text[i].ToString(), out int k)) //переделать
+                    {
+                        str.Add(Char.Parse(k.ToString())); 
+                    }
+                    else
+                    {
+                        if (_text[i] == '(')
+                            stac.Add(_text[i]);
+                        if(_text[i] == ')')
+                        {
+                            if (stac[stac.Count - 1] == '(')
+                                stac.RemoveAt(stac.Count - 1);
+                            else
+                            {
+                                str.Add(stac[stac.Count - 1]);
+                                stac.RemoveAt(stac.Count - 1); //написать проверку на колличество скобок
+                            }
 
+                        }
+                        else
+                        {
+                            if(stac[stac.Count - 1] == '(' || stac.Count == 0)
+                            {
+                                stac.Add(_text[i]);
+                            }
+
+                            else
+                            {
+                                int p = Priority(_text[i]);
+                                int pleft = Priority(stac[stac.Count - 1]);
+                                if (pleft >= p)
+                                {
+                                    str.Add(stac[stac.Count - 1]);
+                                    stac.RemoveAt(stac.Count - 1);
+                                }
+                                else
+                                    stac.Add(_text[i]);
+                                
+                            }
+                        }
+                       
+                    }
+                }
+
+
+                for(int i = stac.Count - 1; i >= 0; i--)
+                {
+
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Неправильная запись!");
+            }
         }
     }
 }
