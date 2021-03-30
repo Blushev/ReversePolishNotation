6f5d322f5f06a54e726321403d2bd58dc4873900
Egor <egor.rumyancev.9494@mail.ru>
2021-03-30 15:48:35 +0500

    Добавлен метод расчета приоритета знаков

---
+++ ОПЗ/GetOPS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ОПЗ
+{
+    class GetOPS
+    {
+        public int Priority(char _prioritySign)
+        {
+            int priority = 0;
+            switch (_prioritySign)
+            {
+                case '+':
+                case '-':
+                    {
+                        priority = 1;
+                        break;
+                    }
+
+                case '*':
+                case '/':
+                    {
+                        priority = 2;
+                        break;
+                    }
+
+                case '^':
+                    {
+                        priority = 3;
+                        break;
+                    }
+            }
+            return priority;
+        }
+
+
+        public void SignAfter()
+        {
+
+        }
+    }
+}
